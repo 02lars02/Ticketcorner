d1066a4b77874ac885abbc3022965b96c59e3a1e
Luca Bucher <lucbu01@bluewin.ch>
2019-04-16 23:02:00 +0200

    switch Validator to right place

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,8 +3,8 @@ require 'core/helpers.php';
 require 'core/database.php';
 
 require 'core/Router.php';
+require 'core/Validator.php';
 
 require 'app/Models/TicketBuy.php';
 require 'app/Models/Bonus.php';
 require 'app/Models/Concert.php';
-require 'app/Models/Validator.php';
