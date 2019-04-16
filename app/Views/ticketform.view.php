a78bbbcb5f85ffe7f3db81cdb7ab55bd3aec700e
Luca Bucher <luca.bucher@bison-group.com>
2019-04-16 16:15:27 +0200

    see term in view

--- app/Views/ticketform.view.php
+++ app/Views/ticketform.view.php
@@ -11,7 +11,7 @@
 <div class="topbar">
     <h2><?= $isEdit ? 'Ticketkauf bearbeiten' : 'Ticketkauf erfassen' ?></h2>
 </div>
-<form action="<?= $isEdit ? 'editTicket' : 'addTicket' ?>" method="post">
+<form id="form" action="<?= $isEdit ? 'editTicket' : 'addTicket' ?>" method="post">
     <input type="hidden" name="id" value="<?=$ticketBuy->id?>">
     <label for="name">Name</label>
     <input type="text" id="name" name="name" required class="form-control" value="<?= $ticketBuy->name ?? '' ?>"> <br>
@@ -22,9 +22,11 @@
     <label for="bonus">Treuebonus</label>
     <select id="bonus" name="bonus" id="bonus" class="form-control">
         <?php foreach ($bons as $oneBon): ?>
-            <option value="<?= $oneBon->id ?>" <?php if(isset($ticketBuy) && $oneBon->id == $ticketBuy->bonus->id) { ?> selected <?php }?>><?= $oneBon->text ?></option>
+            <option value="<?= $oneBon->id ?>" termReduction="<?= $oneBon->termReduction ?>" <?php if(isset($ticketBuy) && $oneBon->id == $ticketBuy->bonus->id) { ?> selected <?php }?>><?= $oneBon->text ?></option>
         <?php endforeach; ?>
     </select><br>
+    <label for="term">Zahlungsfrist</label>
+    <input type="text" id="term" name="term" required class="form-control" disabled> <br>
     <label for="concert">Konzert</label>
     <select id="concert" name="concert" class="form-control">
         <?php foreach ($concerts as $oneConcert): ?>
@@ -33,11 +35,29 @@
     </select><br>
     <?php if($isEdit) { ?>
         <input class="checkbox" type="checkbox" id="paid" name="paid" <?php if($ticketBuy->paid == true){?> checked <?php }?>>
+        <label for="paid">Bezahlt</label><br>
     <?php } ?>
-    <label for="paid">Bezahlt</label><br>
     <input class="btn btn-primary" type="submit" value="Speichern">
     <button class="btn btn-default" onclick="window.location.href='notpaid'">Abbrechen</button>
 </form>
+<script>
+    $(document).ready(function(){
+        var createDate = <?= isset($ticketBuy) ? "new Date('" . $ticketBuy->createDate->format(DateTimeInterface::ISO8601) . "')" : "new Date()" ?>;
+        updateTermDate();
 
+        $("#form").submit(function(event){
+            
+        });
+
+        $('#bonus').change(updateTermDate);
+
+        function updateTermDate(){
+            var termDate = new Date(createDate);
+            termDate.setDate(createDate.getDate() + 30 - (bonus.options[bonus.selectedIndex].getAttribute("termReduction")));
+            $("#term").attr('value', termDate.toLocaleDateString());
+        }
+    });
+    
+</script>
 </body>
 </html>
